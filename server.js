42915aca5b9aab1717a04e9792e68b392aec86a7
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-08 17:43:11 -0400

    server goes to index page

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
