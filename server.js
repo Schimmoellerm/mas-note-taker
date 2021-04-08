19a442cd887557d3a3f6c31b8c78d049c6095160
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-08 17:51:23 -0400

    server now will transition to notes.html

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
