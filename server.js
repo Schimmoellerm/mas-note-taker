42cdd01ab86a0e33585e8b5fd5b01e854fd7bd8c
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-10 10:00:42 -0400

    can post notes, need to retrieve note information with unique ids

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
-
+const fs = require('fs');
+const DB = require('./db/db.json')
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(DB));
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    console.log(newNote);
+
+//create unique ids for newNote (property called id)
+
+
+    DB.push(newNote);
+    console.log(DB)
+
+    fs.writeFile('./db/db.json', JSON.stringify(DB), function(err){
+        if (err) throw err;        
+        
+        console.log('Wrote to File')
+    });
+    res.json(newNote)
+    
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
